07b21cfd800201c99c8990c45200992e065e2e0a
Christopher Barnes <chris.barnes.2000@me.com>
2022-07-05 09:18:13 -0700

    [Add]: Dockerfile

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -7,14 +7,21 @@ app = Flask(__name__)
 def index():
     return "<h1>Welcome To Your Spy Journal!</h1>"
 
+
 @app.route("/audio/")
 def audio():
     return "<h1>Audio Steganography Configuration Coming Soon!</h1>"
+
 
 @app.route("/visual/")
 def visual():
     return "<h1>Visual Steganography Configuration Coming Soon!</h1>"
 
 
+@app.route("/health.json")
+def health():
+    return jsonify({"status": "UP"}), 200
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
